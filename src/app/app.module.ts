739d6cba1b03baf67cd5c4131567ecdc26c1d0cd
apspot <aradi.peter.spot@gmail.com>
2017-04-28 20:39:54 +0200

    Init Angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyA3gLvJjnDuNQiTX8h3BoXnIN3qS37sECA",
+    authDomain: "hello-angular-fire.firebaseapp.com",
+    databaseURL: "https://hello-angular-fire.firebaseio.com",
+    projectId: "hello-angular-fire",
+    storageBucket: "hello-angular-fire.appspot.com",
+    messagingSenderId: "498914816395"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
