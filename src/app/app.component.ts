d7faef8b0f44b29b8d4bb3c45f0b0af74f7a5e46
apspot <aradi.peter.spot@gmail.com>
2017-04-29 16:15:17 +0200

    Joining

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,22 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 export class AppComponent {
   title = 'app works!';  
   cuisines: FirebaseListObservable<any[]>;
-  restaurants: FirebaseListObservable<any[]>;
+  restaurants: Observable<any[]>;
   restaurant;
 
   constructor(private af: AngularFire) {}
 
   ngOnInit() {
     this.cuisines = this.af.database.list('/cuisines');
-    this.restaurants = this.af.database.list('/restaurants');
+    this.restaurants = this.af.database.list('/restaurants')
+      .map(restaurants => {
+        console.log("BEFORE MAP", restaurants);
+        restaurants.map(restaurant => {
+          restaurant.cuisineType = this.af.database.object('cuisines/' + restaurant.cuisine);
+        });
+        console.log("AFTER MAP", restaurants);
+        return restaurants;
+      });
     this.restaurant = this.af.database.object('/restaurant');
   }
 
